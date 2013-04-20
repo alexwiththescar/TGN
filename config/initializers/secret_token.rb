4e51a7ac59cb82c6a9d4fa5aa5f59474425fed63
Alexwiththescar <thesalesstar@gmail.com>
2013-04-20 13:01:58 +0200

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tgn::Application.config.secret_token = '27278189151b16914b1d02929f35b00db03eb66c5ee06e121da45db6d087dba4022db83f64e475bf556c6d43c4f7390f4c2d7936109fe3a734bc0268c9fc4ec7'
